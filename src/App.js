b672ed666420f917b99ab593ed08de3f53d95ffb
Tunahanclr <tunaceler@gmail.com>
2023-02-01 20:04:45 +0300

    todolist

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [todos,setTodos]=useState([])
-    const deleteTodo=(id)=>{
-      setTodos(todos.filter((todo,index)=>index !== id ))
-    }
+   
     return (
     <div className="App">
       <ToastContainer/>
       <h1>Todo</h1>
         <TodoForm todos={todos} setTodos={setTodos}/>
-        <TodoList  todos={todos} deleteTodo={deleteTodo}/>
+        <TodoList  todos={todos} setTodos={setTodos} />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import {GiCancel} from 'react-icons/gi'
+import React, { useState } from 'react';
+import { GiCancel } from 'react-icons/gi';
 
-function TodoList({todos,deleteTodo}) {
+function TodoList({ todos,setTodos }) {
+  const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className='todo-container'>
-          
-            {todos.map((todo,index)=>(
-                <div key={index} className='todo-items'>
-                        <p >{todo}</p>
-                        <i  onClick={()=>{
-                                deleteTodo()
-                        }} className='circle  bi bi-x'></i>
-                </div>
-                    ))}
-            
-                 
-            </div>
-  )
+      {todos.map((todo, index) => (
+        <div key={index} className='todo-items'>
+          <p>{todo}</p>
+          <i
+            className='circle bi bi-x'
+            onClick={() => handleDelete(index)}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
+
+
+
